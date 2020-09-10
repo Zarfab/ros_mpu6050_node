c715b7eb4c57b9ee87564f089f62fe26ae7633ba
unknown <fabien.grisard@gmail.com>
2020-09-10 23:49:56 +0200

    init i2cdev before mpu

--- utils/IMU_zero.cpp
+++ utils/IMU_zero.cpp
@@ -33,7 +33,7 @@ To compile on a Raspberry Pi (1 or 2 or 3)
       $ PATH_I2CDEVLIB=/usr/share/arduino/libraries/i2cdevlib/
       $ gcc -o IMU_zero IMU_zero.cpp \
          -I ${PATH_I2CDEVLIB}RaspberryPi_bcm2835/I2Cdev ${PATH_I2CDEVLIB}RaspberryPi_bcm2835/I2Cdev/I2Cdev.cpp \
-         -I ${PATH_I2CDEVLIB}/Arduino/MPU6050/ ${PATH_I2CDEVLIB}/Arduino/MPU6050/MPU6050.cpp -l bcm2835 -l m
+         -I ${PATH_I2CDEVLIB}Arduino/MPU6050/ ${PATH_I2CDEVLIB}Arduino/MPU6050/MPU6050.cpp -l bcm2835 -l m
       $ sudo ./IMU_zero
 
 If an MPU6050 
@@ -159,11 +159,17 @@ void Initialize()
 {
 	// initialize device
 	printf("Initializing I2C devices...\n");
-	accelgyro.initialize();
+	I2Cdev::initialize();
 
 	// verify connection
 	printf("Testing device connections...\n");
-	printf(accelgyro.testConnection() ? "MPU6050 connection successful\n" : "MPU6050 connection failed\n");
+	if(!accelgyro.testConnection()) {
+		printf("MPU6050 connection failed\n");
+		return;
+
+	printf("MPU6050 connection successful\n"); 
+	accelgyro.initialize();
+	printf("Initialization done!\n");
 } // Initialize
 
 void SetAveraging(int NewN)
