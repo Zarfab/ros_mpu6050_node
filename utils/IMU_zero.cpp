27899ccf3c0bb19cde2b85b28032558883324269
unknown <fabien.grisard@gmail.com>
2020-09-10 13:33:46 +0200

    change compilation line

--- utils/IMU_zero.cpp
+++ utils/IMU_zero.cpp
@@ -30,11 +30,11 @@ To compile on a Raspberry Pi (1 or 2 or 3)
   3. enable i2c on your RPi , see https://learn.adafruit.com/adafruits-raspberry-pi-lesson-4-gpio-setup/configuring-i2c
   4. connect your i2c devices
   5. then from bash
-      $ PATH_I2CDEVLIB=~/i2cdevlib/
-      $ gcc -o MPU6050_example_1 ${PATH_I2CDEVLIB}RaspberryPi_bcm2835/MPU6050/examples/MPU6050_example_1.cpp \
+      $ PATH_I2CDEVLIB=/usr/share/arduino/libraries/i2cdevlib/
+      $ gcc -o IMU_zero IMU_zero.cpp \
          -I ${PATH_I2CDEVLIB}RaspberryPi_bcm2835/I2Cdev ${PATH_I2CDEVLIB}RaspberryPi_bcm2835/I2Cdev/I2Cdev.cpp \
          -I ${PATH_I2CDEVLIB}/Arduino/MPU6050/ ${PATH_I2CDEVLIB}/Arduino/MPU6050/MPU6050.cpp -l bcm2835 -l m
-      $ sudo ./MPU6050_example_1
+      $ sudo ./IMU_zero
 
 If an MPU6050 
       * is an ideal member of its tribe, 
